e25fdc4fcb5f0b2b6845cb8164dd8fb53799d6a2
Simon Sørensen <fynboen1999@gmail.com>
2019-04-24 21:10:22 +0200

    Update fims_projekt.ino

--- fims_projekt.ino
+++ fims_projekt.ino
@@ -8,51 +8,94 @@
 //Arduino Mega, til ekstra lageringsplads
 //SD-kort læser, til at gemme filer og data
 
-//Bibliotek til brug af grafik, kan være hardware-specifikke (Må ikke opdateres uden videre)
-#include <Adafruit_GFX.h>    
-#include <MCUFRIEND_kbv.h>
-MCUFRIEND_kbv tft;
+#include <usbhid.h>
+#include <usbhub.h>
+#include <hiduniversal.h>
+#include <hidboot.h>
+#include <SPI.h>
+#include <LiquidCrystal.h>
+#include <DS3231.h>
 
-bool printText = true;
+const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+DS3231 rtc(SDA, SCL);
 
-//16-bit farve værdier som læselige variabler
-#define BLACK   0x0000
-#define BLUE    0x001F
-#define RED     0xF800
-#define GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
+class MyParser : public HIDReportParser {
+  public:
+    MyParser();
+    void Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf);
+  protected:
+    uint8_t KeyToAscii(bool upper, uint8_t mod, uint8_t key);
+    virtual void OnKeyScanned(bool upper, uint8_t mod, uint8_t key);
+    virtual void OnScanFinished();
+};
 
-uint16_t g_identifier;
+MyParser::MyParser() {}
 
-//Øverst venstre fra USB port
-const int fims_rotation = 3;
-
-int x = 1;
-
-void setup(void) {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
-  tft.begin(0x65);
-  tft.setRotation(3);
+void MyParser::Parse(USBHID *hid, bool is_rpt_id, uint8_t len, uint8_t *buf) {
+  // If error or empty, return
+  if (buf[2] == 1 || buf[2] == 0) return;
+  for (uint8_t i = 7; i >= 2; i--) {
+    // If empty, skip
+    if (buf[i] == 0) continue;
+    // If enter signal emitted, scan finished
+    if (buf[i] == UHS_HID_BOOT_KEY_ENTER) {
+      OnScanFinished();
+    }
+    // If not, continue normally
+    else {
+      // If bit position not in 2, it's uppercase words
+      OnKeyScanned(i > 2, buf, buf[i]);
+    }
+    return;
+  }
 }
 
-void loop(void) {
-  // put your main code here, to run repeatedly:
-  fimsText();
+uint8_t MyParser::KeyToAscii(bool upper, uint8_t mod, uint8_t key) {
+  // Letters
+  if(VALUE_WITHIN(key, 0x04, 0x1d)) {
+    if(upper) return (key - 4 + 'A');
+    else return (key - 4 + 'a');
+  }
+  //Numbers
+  else if (VALUE_WITHIN(key, 0x1e, 0x27)) {
+    return ((key == UHS_HID_BOOT_KEY_ZERO) ? '0' : key - 0x1e + '1');
+  }
+  return 0;
 }
 
+void MyParser::OnKeyScanned(bool upper, uint8_t mod, uint8_t key) {
+  uint8_t ascii = KeyToAscii(upper, mod, key);
+  Serial.print((char)ascii);
+  lcd.print((char)ascii);
+}
 
+void MyParser::OnScanFinished() {
+  lcd.setCursor(0, 1);
+  lcd.print("Varer scannet!");
+  Serial.println(" - Finished");
+}
 
-unsigned long fimsText() {
-  tft.setRotation(fims_rotation);
-  tft.setCursor(tft.width() / 5, tft.height() / 2.5);
-  tft.fillScreen(BLACK);
-  tft.setTextColor(WHITE);
-  tft.setTextSize(4);
-  tft.print("I <3 E :^)");
-  delay(3000);
+USB Usb;
+USBHub Hub(&Usb);
+HIDUniversal Hid(&Usb);
+MyParser Parser;
+
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(115200);
+  rtc.begin();
+  if (Usb.Init() == -1) {
+    Serial.println("OSC did not start.");
+  }
+  delay(200);
+  Hid.SetReportParser(0, &Parser);
+}
+
+void loop() {
+  Usb.Task();
+  lcd.setCursor(0, 0);
+  lcd.print(rtc.getDateStr());
+  lcd.print(" ");
+  lcd.print(rtc.getTimeStr());
 }
